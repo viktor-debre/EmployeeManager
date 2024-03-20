39f2b7fafbaf39d898a37a07de94e33c0ad490b6
Viktor_Debre <viktor.debre2002@gmail.com>
2024-03-20 18:23:20 +0100

    Add project files.

---
+++ EmployeeManager/Controllers/EmployeeController.cs
@@ -0,0 +1,90 @@
+﻿using EmployeeManager.Data;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace EmployeeManager.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase
+{
+    private const string connectionString = "Server=localhost;Database=EployeesDB;Port=5432; User Id=postgres;Password=root";
+
+    [HttpGet]
+    public Employee GetEmployeeById([FromQuery] int employeeId)
+    {
+        Employee rootEmployee = null;
+        var employeeDictionary = new Dictionary<int, Employee>();
+
+        string query = @"
+               WITH RECURSIVE EmployeeHierarchy AS (
+                   SELECT Id, Name, ManagerId, Enabled
+                   FROM Employee
+                   WHERE Id = @EmployeeId
+                   UNION ALL
+                   SELECT e.Id, e.Name, e.ManagerId, e.Enabled
+                   FROM Employee e
+                   INNER JOIN EmployeeHierarchy eh ON e.ManagerId = eh.Id
+               )
+               SELECT Id, Name, ManagerId, Enabled
+               FROM EmployeeHierarchy;";
+
+        using var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        using var command = new NpgsqlCommand(query, connection);
+        command.Parameters.AddWithValue("@EmployeeId", employeeId);
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            var employee = new Employee
+            {
+                Id = (int)reader["Id"],
+                Name = (string)reader["Name"],
+                ManagerId = reader["ManagerId"] as int?,
+                Enabled = (bool)reader["Enabled"]
+            };
+
+            if (rootEmployee == null)
+            {
+                rootEmployee = employee;
+            }
+
+            employeeDictionary.Add(employee.Id, employee);
+        }
+
+        reader.Close();
+
+        foreach (var employee in employeeDictionary.Values)
+        {
+            if (employee.ManagerId.HasValue && employeeDictionary.ContainsKey(employee.ManagerId.Value))
+            {
+                Employee manager = employeeDictionary[employee.ManagerId.Value];
+                manager.Employees.Add(employee);
+            }
+        }
+
+        return rootEmployee;
+    }
+
+    [HttpPut]
+    public void EnableEmployee(
+        [FromQuery] int employeeId,
+        [FromQuery] bool enable)
+    {
+        string updateQuery =
+            @"UPDATE Employee 
+              SET Enabled = @Enable
+              WHERE Id = @EmployeeId";
+
+        using var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        using var command = new NpgsqlCommand(updateQuery, connection);
+        command.Parameters.AddWithValue("@EmployeeId", employeeId);
+        command.Parameters.AddWithValue("@Enable", enable);
+
+        command.ExecuteNonQuery();
+    }
+}
---
+++ EmployeeManager/Data/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManager.Data;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? ManagerId { get; set; }
+    public bool Enabled { get; set; }
+    public List<Employee>? Employees { get; set; } = new List<Employee>();
+}
